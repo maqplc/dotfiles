34a70239fe98c58360a5fae1c82a566757a56356
maqplc <maqplc+github@gmail.com>
2008-09-23 17:09:46 +0200

    Pheeww it's not done but it will be enough

--- setup/install.sh
+++ setup/install.sh
@@ -1,44 +1,85 @@
 #!/bin/sh
 
-progs="xarchiver feh mplayer rxvt-unicode evince thunar firefox snownews sudo gvim ncmpc mpc mpd dzen2 texlive-core nvidia xorg codecs flashplugin alsa-utils hal fam netcfg fakeroot pkgconfig gcstar wine abs"
-
-
-user="maqplc"
-daemons="@net-profiles alsa hal fam"
-
-#Couleurs et pretty print
+#------------------------------
+# COLORS
+#------------------------------
 green="\033[32m"
 magenta="\033[35m"
 red="\033[31m"
 bold="\033[1m"
 normal="\033[0m"
 
-#Fonctions
-net_connect() {
+#------------------------------
+# VARIABLES
+#------------------------------
+
+PROGS="\
+	xorg nvidia xmonad xmonad-contrib dzen2 \
+	openssh \
+	screen \
+	cmus \
+	feh \
+	mplayer codecs \
+	rxvt-unicode \
+	evince \
+	thunar \
+	firefox flash-plugin \
+	snownews \
+	sudo \
+	gvim \
+	alsa-utils \
+	hal \
+	fam \
+	netcfg \
+	abs fakeroot pkgconfig \
+	wine \
+	unzip unrar bzip2 \
+	"
+
+user="maqplc"
+daemons="@net-profiles alsa hal fam"
+
+
+#------------------------------
+# FUNCTIONS 
+#------------------------------
+
+title_display() {
+	echo -e "${bold}=>${green} $1 $normal"
+}
+
+CONNECT() {
 	net_profile="wifiHome"
 	net_dir="/etc/network.d"
 	
-	echo -e "Copie du profil $bold $net_profile $normal dans le dossier $bold $net_dir $normal."
+	title_display "Connexion à internet"	
 	
+	echo -ne "Saisissez l'${bold}ESSID${normal} du réseau cible : "
+	read net_ESSID
+
+	echo -ne "Saisissez la ${bold}clé${normal} du réseau cible :"
+	read net_KEY
+
+	echo 'ESSID="'$net_ESSID'"' >> $net_profile
+	echo 'KEY="'$net_KEY'"' >> $net_profile
+
+	echo -e "Copie du profil $bold $net_profile $normal"
 	cp $net_profile $net_dir
-	netcfg2 wifiHome
+	netcfg2 $net_profile
 }
 
-daemons_update() {
-	sed 's/\(DAEMONS=([^)]*\))/\1 '$daemons')/' /etc/rc.conf > rc.back
-	
-	mv ./rc.back /etc/rc.conf
-}
+UPDATE() {
+	title_display "Mise à jour du système"
 
-system_update() {
-	#pacman -Sy --noconfirm pacman
 	pacman -Sy pacman
-	resolve_conflict
-
+	UNCONFLICT
+	
 	pacman -Syu
 }
 
-resolve_conflict() {
+UNCONFLICT() {
+	title_display "Résolution des conflits"
+
 	echo $(find / -name "*pacnew")
 	for new in $(find / -name "*pacnew")
 	do
@@ -46,12 +87,13 @@ resolve_conflict() {
 		continuer=true
 		while $continuer
 		do
+			clear
 			echo -e "Action pour le fichier $bold $old $normal :"
-			echo -e "${bold}e${normal}diter"
-			echo -e "${bold}d${normal}iff"
+			echo -e "\t ${bold}E${normal}diter"
+			echo -e "\t ${bold}D${normal}iff"
 			
-			echo -e "${bold}c${normal}onserver"
-			echo -e "${bold}r${normal}emplacer"
+			echo -e "\t ${bold}C${normal}onserver l'ancien fichier (supprimer le nouveau)"
+			echo -e "\t ${bold}R${normal}emplacer par le nouveau"
 			
 			read -n 1 choice
 			
@@ -65,37 +107,41 @@ resolve_conflict() {
 	done
 }
 
-#0) Activer l'internet
-net_connect
+POLISH() {
+	title_display "Making things better"
 
-#1) Mettre à jour le système
-system_update
-resolve_conflict
+	echo "Démons à ajouter : $daemons"
+	read insipid_var
+	vi /etc/rc.conf
 
-#2) Installer les programmes
-pacman -S $progs
+	alsamixer
+	alsactl store
 
-#3) Mettre à jour le rc.conf
-daemons_update
+	vi /etc/locale.gen
+	locale-gen
 
-#4) Créer l'utilisateur
-useradd -m -G disk,lp,wheel,mail,log,games,network,video,audio,optical,floppy,power $user
+	visudo
 
-passwd $user 
-tar -C "/home/$user" -xvf user.tar
-chown -R $user:$user "/home/$user"
-chmod -R 600 "/home/$user/"
+	xorgcfg
 
-#5) Commandes supplémentaires
-alsamixer ; alsactl store
-locale-gen
-visudo
-abs
-xorgcfg
+	abs
+}
+
+USER_CREATE() {
+	useradd -m -G disk,lp,wheel,mail,log,games,network,video,audio,optical,floppy,power $user
+	passwd $user 
+}
+
+#------------------------------
+# RUNNING
+#------------------------------
 
 
-echo "Xmonad recompilation"
-echo "run.sh"
-echo "Conky reinstall"
-echo "Reboot"
+#UPDATE
+#UNCONFLICT
 
+#pacman -S $progs
+#USER_CREATE
+#POLISH
+
+echo "Ready for some action : CONNECT UPDATE UNCONFLICT USER_CREATE POLISH and PROGS var"
